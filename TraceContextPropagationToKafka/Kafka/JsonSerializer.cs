4144f69ec56d2c53c532255a8a3abb2898aadc0f
Magdalena Baran <mbaran@microsoft.com>
2021-03-22 14:49:42 +0100

    Add KafkaProducer and its configuration

---
+++ TraceContextPropagationToKafka/Kafka/JsonSerializer.cs
@@ -0,0 +1,21 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace TraceContextPropagationToKafka.Kafka
+{
+    public class JsonSerializer<T> :
+            ISerializer<T>, IDeserializer<T>
+    {
+        public byte[] Serialize(T data, SerializationContext context)
+        {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data));
+        }
+
+        public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+        {
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data));
+        }
+    }
+}
---
+++ TraceContextPropagationToKafka/Kafka/KafkaConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+
+namespace TraceContextPropagationToKafka.Kafka
+{
+    public class KafkaConfiguration
+    {
+        public ClientConfig ClientConfig { get; set; } = new ClientConfig();
+        public KafkaTopics Topics { get; set; } = new KafkaTopics();
+    }
+
+    public class KafkaTopics
+    {
+        public string Demo { get; set; } = null!;
+    }
+}
